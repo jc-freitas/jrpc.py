325f6b6cebe08c14dcb0ee7d628c6833d371e4bf
Júlio Freitas <enige@protonmail.com>
2025-09-29 10:23:13 -0300

    Update author and repository URL in setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ install_requires = {
 }
 setup(name='jrpc.py',
       version='0.1',
-      url='https://github.com/brokeboienige/jrpc.py',
+      url='https://github.com/jc-freitas/jrpc.py',
       license='MIT',
-      author='brokeboienige',
+      author='jc-freitas',
       author_email='enige@protonmail.com',
       description='A easy to use python library for Xx jAmes t xX\'s Xbox 360 JRPC plugin.',
       packages=['jrpc'],
